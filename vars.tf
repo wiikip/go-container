6d62acdb05dea9699ff264b0c82b660db7183d3f
Kevin Fairise <kevin.fairise@student-cs.fr>
2021-04-02 21:41:44 +0200

    Add terraform CI

--- vars.tf
+++ vars.tf
@@ -3,3 +3,22 @@ variable "port" {
  default = "3000"
 }
 
+variable "client_certificate" {
+    type = string
+  
+}
+variable "client_certificate" {
+    type = string
+}
+
+variable "client_key" {
+    type = string
+}
+
+variable "ca_certificate" {
+    type = string
+}
+
+variable "cluster_endpoint"{
+    type = string
+}
