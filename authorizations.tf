087997d14d913b592c0d0e5406d55be6f06c4087
Kevin Fairise <kevin.fairise@student-cs.fr>
2021-04-10 16:27:59 +0200

    authorization on namespace go-container-managed

--- authorizations.tf
+++ authorizations.tf
@@ -1,6 +1,7 @@
 resource "kubernetes_role" "go-container-role" {
   metadata {
-    name = "go-container-role"
+    name      = "go-container-role"
+    namespace = "go-container-managed"
   }
   rule {
     api_groups = [""]
@@ -18,6 +19,7 @@ resource "kubernetes_role_binding" "go-container-rb" {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
     name      = kubernetes_role.go-container-role.metadata.0.name
+    namespace = "go-container-managed"
   }
   subject {
     kind      = "ServiceAccount"
--- namespace.tf
+++ namespace.tf
@@ -2,5 +2,10 @@ resource "kubernetes_namespace" "go-container" {
   metadata {
     name = "go-container"
   }
+}
 
+resource "kubernetes_namespace" "go-container-managed" {
+  metadata {
+    name = "go-container-managed"
+  }
 }
